08b91465d26b0125405c7829471bfc83e9c5e33e
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2017-05-04 16:16:29 +0200

    update proto

--- generate-python.sh
+++ generate-python.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
- [ -z $1 ] && [ -z $2 ] && [ -z $3 ] && {
-   echo "Usage: $0 <proto-path> <out-dir> <proto-file>"
-   echo '    $1 = absolute path to folder where the .proto is'
-   echo '    $2 = absolute output dir'
-   echo '    $3 = absolute path to .proto file'
-   exit 1
- }
+
+[ -z $1 ] && [ -z $2 ] && [ -z $3 ] && {
+    echo "Usage: $0 <proto-path> <out-dir> <proto-file>"
+    echo '    $1 = absolute path to folder where the .proto is'
+    echo '    $2 = absolute output dir'
+    echo '    $3 = absolute path to .proto file'
+    exit 1
+}
 
 python -m pip install grpcio
 python -m pip install grpcio-tools
@@ -15,3 +16,5 @@ python -m pip install grpcio-tools
 # $3 = absolute path to .proto file
 
 python -m grpc_tools.protoc -I$1 --python_out=$2 --grpc_python_out=$2 $3
+
+sed -i "s/import messages_pb2 as messages__pb2/import eu.softfire.tub.messaging.grpc.messages_pb2 as messages__pb2/g" $2/messages_pb2_grpc.py
