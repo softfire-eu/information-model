42c40bacab384c481aa63d89e726dadc0c61c887
Bjoern Riemer <bjoern.riemer@fokus.fraunhofer.de>
2017-05-04 16:08:16 +0200

    added error handling on generation script

--- generate-python.sh
+++ generate-python.sh
@@ -1,3 +1,12 @@
+#!/bin/sh
+ [ -z $1 ] && [ -z $2 ] && [ -z $3 ] && {
+   echo "Usage: $0 <proto-path> <out-dir> <proto-file>"
+   echo '    $1 = absolute path to folder where the .proto is'
+   echo '    $2 = absolute output dir'
+   echo '    $3 = absolute path to .proto file'
+   exit 1
+ }
+
 python -m pip install grpcio
 python -m pip install grpcio-tools
 
